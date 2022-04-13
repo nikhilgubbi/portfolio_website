b9232478809ac8bacfe842e3859dbfe9779e914e
nikhilgubbi <nikhilgubbi@gmail.com>
2022-04-13 16:21:17 +0530

    React Portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,8 +19,17 @@ const Projects = () => (
           <CardInfo>{description}</CardInfo>
           <div>
             <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
 
           </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
         </BlogCard>
       ))}
     </GridContainer>
